da0cb430d1a25c5f84921bcf662ebdec4cd2469b
kooqooo <koogmlcks@gmail.com>
2024-07-16 17:23:08 +0900

    fix: import할 때 실행되지 않도록 수정
    #1

--- utils/path_finders.py
+++ utils/path_finders.py
@@ -21,16 +21,17 @@ def find_project_path_pathlib(start_path='.'):
             return str(Path(root).resolve())
     return None
 
-try:
-    project_path = find_project_path()
-    if project_path:
-        print(f'.git 폴더가 있는 프로젝트 경로: {project_path}')
-    else:
-        print('.git 폴더를 찾을 수 없습니다.')
-    project_path = find_project_path_pathlib()
-    if project_path:
-        print(f'.git 폴더가 있는 프로젝트 경로: {project_path}')
-    else:
-        print('.git 폴더를 찾을 수 없습니다.')
-except ValueError as e:
-    print(e)
+if __name__ == "__main__":
+    try:
+        project_path = find_project_path()
+        if project_path:
+            print(f'.git 폴더가 있는 프로젝트 경로: {project_path}')
+        else:
+            print('.git 폴더를 찾을 수 없습니다.')
+        project_path = find_project_path_pathlib()
+        if project_path:
+            print(f'.git 폴더가 있는 프로젝트 경로: {project_path}')
+        else:
+            print('.git 폴더를 찾을 수 없습니다.')
+    except ValueError as e:
+        print(e)
